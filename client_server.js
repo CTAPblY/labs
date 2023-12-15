5920d4234bf29dae69b25127e51372b30249fbd6
CTAPblY <rezcov02@mail.ru>
2023-12-15 14:36:37 +0300

    add data base table on web page

--- client_server.js
+++ client_server.js
@@ -1,45 +1,46 @@
 var http = require("http");
 var sqlite3 = require("sqlite3");
-
 let db;
 db = new sqlite3.Database('./node_db.db');
-let number = 0;
-var list  = [];
+var fs = require("fs");
 
-
-http.createServer(async function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-    response.write("<!DOCTYPE html>\n" +
-                    "<html>\n" +
-                    " <head>\n" +
-                    " <meta charset='utf-8'>\n" +
-                    " </head>\n" +
-                    " <body>\n"
-                );
-
-    function getNumber() {
-        return new Promise((resolve, reject) => {          
-        let sql = `SELECT * FROM NUMB ORDER BY id`;   
-        return db.all(sql, [], function (err, rows) { 
-            if (err) {
-            console.error(err.message);
-            return reject(err.message);
-            }
-    
-            rows.forEach((row) => {
-                list[row.id] = `${row.id}  ${row.numb}  ${row.country}`;
+http.createServer(function (request, response) {
+    fs.readFile('db_table.html', async function(err, data){
+        response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+        response.write(data);
+        function getNumber() {
+            return new Promise((resolve, reject) => {          
+                let sql = `SELECT * FROM NUMB ORDER BY id`;   
+                return db.all(sql, [], function (err, rows) { 
+                    var list  = [];
+                    if (err) {
+                    console.error(err.message);
+                    return reject(err.message);
+                    }
+                    
+                    rows.forEach((row) => {
+                        list.push(row);
+                    });
+                    return resolve(list);
+                });
             });
-            return resolve(list);
-        });
-        });
-    }
+        }
 
-    let result = await getNumber();
-    response.write(`${result}`);
-    response.end(
-                " </body>\n" +
-                "</html>\n"
-                );
+        var result = await getNumber();
+        for (var i = 0; i < result.length; i++){
+            if (i % 2 != 0){
+                response.write('<div class="row border border-dark">\n');
+            }   else { response.write('<div class="row border border-dark bg-info">\n'); }
+            response.write('<div class="col border border-dark">' + `${result[i].id}` + '</div>\n');
+            response.write('<div class="col border border-dark">' + `${result[i].numb}` + '</div>\n');
+            response.write('<div class="col border border-dark">' + `${result[i].country}` + '</div>\n');
+            response.write('</div>\n');
+        }
+            
+        response.end('</div>\n' + '</body>\n'  + '</html>\n');
+    });
 }).listen(3000);
 console.log("Server running at http://localhost:3000/");
+
+
 
